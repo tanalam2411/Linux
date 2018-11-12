c779254cf94e6d56c1769a5d4b93abd9ea44d3c9
Tanveer Alam <tanalam2411@users.noreply.github.com>
2018-11-12 19:10:02 +0530

    Docker installation script for ubuntu 16.04

---
+++ install_docker_ubuntu_16.4.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+sudo_password=""
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo $sudo_password | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo $sudo_password | sudo apt-get update -y
+
+apt-cache policy docker-ce
+
+
+echo $sudo_password | sudo apt-get install -y docker-ce
+
+echo $sudo_password | sudo usermod -aG docker ${USER}
+
+su - ${USER}
+
+id -nG
+
+sudo usermod -aG docker ${USER}
+
+docker info
